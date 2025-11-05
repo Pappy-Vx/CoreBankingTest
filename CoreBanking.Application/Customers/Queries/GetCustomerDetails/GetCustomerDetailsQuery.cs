124f1c0f4509c7454a083c69446c0a23d60c287b
Kolawole Kolapo <pappy.kay9@gmail.com>
2025-11-05 14:30:04 +0100

    Account Quenry Handler Solved

--- CoreBanking.Application/Customers/Queries/GetCustomerDetails/GetCustomerDetailsQuery.cs
+++ CoreBanking.Application/Customers/Queries/GetCustomerDetails/GetCustomerDetailsQuery.cs
@@ -1,8 +1,57 @@
-﻿using CoreBanking.Application.Common.Interfaces;
+﻿using CoreBanking.Application.Accounts.Queries.GetAccountSummary;
+using CoreBanking.Application.Common.Interfaces;
+using CoreBanking.Application.Common.Models;
+using CoreBanking.Core.Interfaces;
+using CoreBanking.Core.ValueObjects;
+using MediatR;
 
 namespace CoreBanking.Application.Customers.Queries.GetCustomerDetails;
 
 public record GetCustomerDetailsQuery : IQuery<CustomerDetailsDto>
 {
     public Guid CustomerId { get; init; }
+}
+
+//public class GetCustomersDetailsQueryHandler : IRequestHandler<GetCustomerDetailsQuery, Result<CustomerDetailsDto>>
+public class GetCustomersDetailsQueryHandler : IRequestHandler<GetCustomerDetailsQuery, Result<CustomerDetailsDto>>
+{
+    private readonly ICustomerRepository _customerRepository;
+
+    public GetCustomersDetailsQueryHandler(ICustomerRepository customerRepository)
+    {
+        _customerRepository = customerRepository;
+    }
+
+    public async Task<Result<CustomerDetailsDto>> Handle(GetCustomerDetailsQuery request, CancellationToken cancellationToken)
+    {
+
+        var customer = await _customerRepository.GetByIdAsync(CustomerId.Create(request.CustomerId));
+        if (customer == null) throw new ArgumentException("Customer doesn't exist");
+
+        var customerDtos = new CustomerDetailsDto
+        {
+            CustomerId = customer.CustomerId,
+            FirstName = customer.FirstName,
+            LastName = customer.LastName,
+            Email = customer.Email,
+            Phone = customer.PhoneNumber,
+            //Address = customer.Address,
+            //DateOfBirth = customer.DateOfBirth,
+            DateRegistered = customer.DateCreated,
+            IsActive = customer.IsActive,
+            Accounts = customer.Accounts.Select(account => new AccountSummaryDto
+            {
+                AccountNumber = account.AccountNumber,
+                AccountType = account.AccountType.ToString(),
+                DisplayName = $"{customer.FirstName} {customer.LastName}",
+                Balance = account.Balance.Amount,
+                Currency = account.Balance.Currency,
+                IsActive = account.IsActive,
+                DateOpened = account.DateOpened
+            }).ToList()
+        };
+
+        return Result<CustomerDetailsDto>.Success(customerDtos);
+    }
+
 }
--- CoreBanking.Application/Customers/Queries/GetCustomers/GetCustomersQuery.cs
+++ CoreBanking.Application/Customers/Queries/GetCustomers/GetCustomersQuery.cs
@@ -1,5 +1,51 @@
-﻿using CoreBanking.Application.Common.Interfaces;
+﻿using CoreBanking.Application.Accounts.Queries.GetAccountSummary;
+using CoreBanking.Application.Common.Interfaces;
+using CoreBanking.Application.Common.Models;
+using CoreBanking.Core.Interfaces;
+using MediatR;
 
 namespace CoreBanking.Application.Customers.Queries.GetCustomers;
 
 public record GetCustomersQuery : IQuery<List<CustomerDto>>;
+
+//public class GetCustomersQueryHandler : IRequestHandler<GetCustomersQuery, Result<List<CustomerDto>>>
+public class  GetCustomersQueryHandler : IRequestHandler<GetCustomersQuery, Result<List<CustomerDto>>>
+
+{
+    private readonly ICustomerRepository _customerRepository;
+
+    public GetCustomersQueryHandler(ICustomerRepository customerRepository)
+    {
+        _customerRepository = customerRepository;
+    }
+
+    public async Task<Result<List<CustomerDto>>> Handle(GetCustomersQuery request, CancellationToken cancellationToken)
+    {
+
+        var customers = await _customerRepository.GetAllAsync();
+
+        var customerDtos = customers.Select(customer => new CustomerDto
+        {
+            CustomerId = customer.CustomerId,
+            FirstName = customer.FirstName,
+            LastName = customer.LastName,
+            Email = customer.Email,
+            Phone = customer.PhoneNumber,
+            DateRegistered = customer.DateCreated,
+            IsActive = customer.IsActive,
+            //Accounts = customer.Accounts.Select(account => new AccountSummaryDto
+            //{
+            //    AccountNumber = account.AccountNumber,
+            //    AccountType = account.AccountType.ToString(),
+            //    DisplayName = $"{customer.FirstName} {customer.LastName}",
+            //    Balance = account.Balance.Amount,
+            //    Currency = account.Balance.Currency,
+            //    IsActive = account.IsActive,
+            //    DateOpened = account.DateOpened
+            //}).ToList()
+        }).ToList();
+
+        return Result<List<CustomerDto>>.Success(customerDtos);
+    }
+
+}
